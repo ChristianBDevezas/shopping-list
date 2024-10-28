91a4d208eb17a76ec10c87c578117b069bb1891f
ChristianBDevezas <christianbdevezas@hotmail.com>
2024-10-28 10:40:29 -0300

    update main.js

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 // 1) Delete items
-let groceryListUl = document.querySelector('#grocery-list ul');
+const groceryListUl = document.querySelector('#grocery-list ul');
 console.log(groceryListUl);
 
 groceryListUl.addEventListener('click', remove);
 // define our handler function
 function remove(e) {
-    let target = e.target;
+    const target = e.target;
     // if(target.classList.contains("delete"))
     if(target.className == 'delete') {
         // target.parentElement.remove();
-        let li = target.parentElement;
+        const li = target.parentElement;
         li.remove();
     }
 };
 
 // 2) Hide items
-let checklabel = document.querySelector('#check label');
-let checkbox = document.querySelector('#hide');
-let groceryList = document.getElementById('grocery-list');
+const checklabel = document.querySelector('#check label');
+const checkbox = document.querySelector('#hide');
+const groceryList = document.getElementById('grocery-list');
 
 checkbox.addEventListener('change', () => {
     if(checkbox.checked) {
@@ -32,14 +32,14 @@ checkbox.addEventListener('change', () => {
 
 // 3) Check if item already exists on the list
 function checkText(inputText) {
-    let listItems = document.querySelectorAll('.items-list li');
-    let listItemsArray = Array.from(listItems);
+    const listItems = document.querySelectorAll('.items-list li');
+    const listItemsArray = Array.from(listItems);
     let textExists = false;
 
     listItemsArray.forEach((item) => {
-        let itemName = item.firstElementChild.innerText;
-        let itemNameLower = itemName.toLowerCase();
-        let inputTextLower = inputText.toLowerCase();
+        const itemName = item.firstElementChild.innerText;
+        const itemNameLower = itemName.toLowerCase();
+        const inputTextLower = inputText.toLowerCase();
 
         if(itemNameLower == inputTextLower) {
             alert("Item already exists!");
@@ -51,18 +51,18 @@ function checkText(inputText) {
 };
 
 // 4) Add items
-let formAdd = document.getElementById('add-item');
+const formAdd = document.getElementById('add-item');
 // attach event listener
 formAdd.addEventListener('submit', (e) => {
     e.preventDefault();
     
     // global variable "groceryListUl" may be used
-    let ul = document.querySelector('#grocery-list ul');
+    const ul = document.querySelector('#grocery-list ul');
 
     // grab user's input text
     // let inputText = formAdd.querySelector('input[type="text"]').value;
-    let inputText = formAdd.querySelector('input[type="text"]');
-    let inputTextValue = inputText.value;
+    const inputText = formAdd.querySelector('input[type="text"]');
+    const inputTextValue = inputText.value;
     console.log(inputTextValue);
 
     if(inputTextValue == "" || inputTextValue.length < 2) {
@@ -78,12 +78,12 @@ formAdd.addEventListener('submit', (e) => {
             inputText.value = null;
 
             // create the list items dynamically
-            let li = document.createElement('li');
-            let groceryName  = document.createElement('span');
-            let groceryInput = document.createElement('input');
+            const li = document.createElement('li');
+            const groceryName  = document.createElement('span');
+            const groceryInput = document.createElement('input');
             groceryInput.type = 'text';
-            let editBtn = document.createElement("span");
-            let deleteButton = document.createElement('span');
+            const editBtn = document.createElement("span");
+            const deleteButton = document.createElement('span');
 
             // add text to the created li element
             groceryName.textContent = inputTextValue;
@@ -116,26 +116,26 @@ const search = document.getElementById('search-item');
 // add event listener
 search.addEventListener('keyup', (e) => {
     // grab the user's text and convert it to lower case
-    let text = e.target.value;
-    let textLowerCase = text.toLowerCase();
+    const text = e.target.value;
+    const textLowerCase = text.toLowerCase();
     // console.log(textLowerCase);
 
     // let's grab each li tag (3 ways)
-    // let groceries = document.getElementsByTagName('li');
-    let groceries = document.querySelectorAll('#grocery-list li');
-    // let groceries = groceryList.querySelectorAll('li');
+    // const groceries = document.getElementsByTagName('li');
+    const groceries = document.querySelectorAll('#grocery-list li');
+    // const groceries = groceryList.querySelectorAll('li');
     // console.log(groceries);
 
     // let's convert the groceries to an array, so we can access the forEach method
-    let groceriesArray = Array.from(groceries);
+    const groceriesArray = Array.from(groceries);
 
     // loop through each grocery item
     groceriesArray.forEach((item) => {
-        let groceryName = item.firstElementChild.textContent;
+        const groceryName = item.firstElementChild.textContent;
         console.log(groceryName);
 
         // convert our text to lower case
-        let groceryNameLower = groceryName.toLowerCase();
+        const groceryNameLower = groceryName.toLowerCase();
         // console.log(groceryNameLower);
 
         // now we can use indexOf to see if text can be found within our grocery list.
@@ -159,19 +159,19 @@ search.addEventListener('keyup', (e) => {
 // 6) Edit items
 groceryListUl.addEventListener('click', edit);
 function edit(e) {
-    let target = e.target;
+    const target = e.target;
 
     if(target.classList.contains('edit')) {
-        let li = e.target.parentNode;
-        let textItem = li.firstElementChild;
-        let inputItem = textItem.nextElementSibling;
-        let editBtn = inputItem.nextElementSibling;
+        const li = e.target.parentNode;
+        const textItem = li.firstElementChild;
+        const inputItem = textItem.nextElementSibling;
+        const editBtn = inputItem.nextElementSibling;
 
         textItem.style.display = 'none';
         inputItem.style.display = 'block';
         editBtn.style.display = 'none';
 
-        let text = textItem.textContent;
+        const text = textItem.textContent;
 
         inputItem.focus();
 
